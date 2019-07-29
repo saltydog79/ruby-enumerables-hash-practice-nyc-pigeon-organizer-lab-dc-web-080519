e126dada8e30adbaa836d34a5fa083a2f1ee89e9
Emily G <aspentree79@gmail.com>
2019-07-29 16:42:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,37 +1,20 @@
 require "pry"
-
 def nyc_pigeon_organizer(data)
+  hash = {}
+data.each do | key, value |
+value.each do | attribute, names |
+  names.each do | name |
+    if !hash[name]
+      hash[name] = {}
+    end
+    if !hash[name][key]
+      hash[name][key] = []
+    end
+
+hash[name][key] << attribute.to_s
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def nyc_pigeon_organizer(data)
-#   hash = {}
-# data.each do | key, value |
-# value.each do | attribute, names |
-#   names.each do | name |
-#     if !hash[name]
-#       hash[name] = {}
-#     end
-#     if !hash[name][key]
-#       hash[name][key] = []
-#     end
-#
-# hash[name][key] << attribute.to_s
-#
-# end
-# end
-# end
-# return hash
-# end
+end
+end
+return hash
+end
